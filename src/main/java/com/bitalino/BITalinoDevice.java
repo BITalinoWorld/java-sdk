7d5471d4f4467447c428b265c5c877904d46a46c
Paulo Pires <pjpires@gmail.com>
2013-09-17 17:41:48 +0100

    First attempt at refactoring BITalino base code.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

---
+++ src/main/java/com/bitalino/BITalinoDevice.java
@@ -0,0 +1,196 @@
+package com.bitalino;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+
+/**
+ * This class represents a BITalino device.
+ */
+public class BITalinoDevice {
+  private final int[] analogChannels;
+  private final String mac;
+  private final int samplerate;
+  private final int totalBytes;
+
+  private StreamConnection conn = null;
+  private DataInputStream dis = null;
+  private DataOutputStream dos = null;
+
+  /**
+   * @param mac
+   *          device's MAC address.
+   * @param samplerate
+   *          the sampling frequency (Hz). Values available are 1000 (default),
+   *          100, 10 and 1.
+   * @param analogChannels
+   *          the analog channels set to read from.
+   * @throws BITalinoException
+   *           if MAC address is invalid.
+   */
+  public BITalinoDevice(final String mac, final int samplerate,
+      final int[] analogChannels) throws BITalinoException {
+    // validate MAC address
+    if (!mac.matches("^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$"))
+      throw new BITalinoException(BITalinoErrorTypes.MACADDRESS_NOT_VALID);
+    this.mac = mac;
+
+    // validate samplerate
+    this.samplerate = samplerate != 1 && samplerate != 10 && samplerate != 100
+        && samplerate != 1000 ? 1000 : this.samplerate;
+
+    // validate analog channels length
+    if (analogChannels.length < 1 || analogChannels.length > 6)
+      throw new BITalinoException(BITalinoErrorTypes.ANALOG_CHANNELS_NOT_VALID);
+    // validate analog channels identifiers
+    for (int channel : analogChannels)
+      if (channel < 0 || channel > 5)
+        throw new BITalinoException(
+            BITalinoErrorTypes.ANALOG_CHANNELS_NOT_VALID);
+    this.analogChannels = analogChannels;
+
+    // calculate totalBytes based on number of used analog channels
+    totalBytes = analogChannels.length <= 4 ? (int) Math
+        .ceil((12f + 10f * analogChannels.length) / 8) : (int) Math
+        .ceil((52f + 6f * (analogChannels.length - 4)) / 8);
+  }
+
+  /**
+   * Connect to BITalino device through Bluetooth identified with the provided
+   * MAC address provided.
+   * <p>
+   * In case the connection is successfully established, let's automatically the
+   * samplerate in the device.
+   */
+  public void open() throws BITalinoException {
+    // connect to bluetooth device
+    try {
+      conn = (StreamConnection) Connector.open("btspp://" + mac + ":1",
+          Connector.READ_WRITE);
+      dis = conn.openDataInputStream();
+      dos = conn.openDataOutputStream();
+      Thread.sleep(2000);
+    } catch (Exception e) {
+      close();
+    }
+
+    // set samplerate on the bluetooth device
+    try {
+      int command = 0;
+      switch (samplerate) {
+      case 1000:
+        command = 0x3;
+        break;
+      case 100:
+        command = 0x2;
+        break;
+      case 10:
+        command = 0x1;
+        break;
+      case 1:
+        command = 0x0;
+        break;
+      }
+      command = (command << 6) | 0x03;
+      write(command);
+    } catch (Exception e) {
+      throw new BITalinoException(BITalinoErrorTypes.SAMPLING_RATE_NOT_DEFINED);
+    }
+  }
+
+  /**
+   * Starts reading predefined analog channels.
+   */
+  public void start() throws Throwable {
+    int bit = 1;
+    for (int channel : analogChannels)
+      bit = bit | 1 << (2 + channel);
+    try {
+      write(bit);
+    } catch (Exception e) {
+      throw new BITalinoException(BITalinoErrorTypes.BT_DEVICE_NOT_CONNECTED);
+    }
+
+  }
+
+  /**
+   * Stop BITalino acquisition.
+   * 
+   * @throws BITalinoException
+   */
+  public void stop() throws BITalinoException {
+    try {
+      write(0);
+      Thread.sleep(2000);
+      close();
+    } catch (Exception e) {
+      throw new BITalinoException(BITalinoErrorTypes.BT_DEVICE_NOT_CONNECTED);
+    }
+  }
+
+  /**
+   * Closes Bluetooth connection and releases any open resources.
+   * 
+   * @throws BITalinoException
+   */
+  private void close() throws BITalinoException {
+    try {
+      conn.close();
+      dis.close();
+      dos.close();
+      conn = null;
+      dis = null;
+      dos = null;
+    } catch (Exception e) {
+      throw new BITalinoException(BITalinoErrorTypes.BT_DEVICE_NOT_CONNECTED);
+    }
+  }
+
+  /**
+   * Writes data to Bluetooth.
+   * 
+   * @param command
+   * @throws BITalinoException
+   */
+  private void write(final int command) throws BITalinoException {
+    BITalinoFrameCodec.write(dos, command);
+  }
+
+  /**
+   * Reads data from Bluetooth.
+   * 
+   * @param numberOfSamples
+   * @return
+   * @throws BITalinoException
+   */
+  public BITalinoFrame[] read(final int numberOfSamples)
+      throws BITalinoException {
+    return BITalinoFrameCodec.read(dis, numberOfSamples, analogChannels,
+        totalBytes);
+  }
+
+  /**
+   * Retrieve device's version.
+   * <p>
+   * <strong>ATTENTION:</strong> Works only in idle mode!
+   */
+  public String version() throws BITalinoException {
+    try {
+      write(7);
+      byte[] version = new byte[30];
+      String test = "";
+      int i = 0;
+      while (!test.equals("\n")) {
+        dis.read(version, i, 1);
+        i++;
+        test = new String(new byte[] { version[i - 1] });
+      }
+      return new String(version);
+    } catch (Exception e) {
+      throw new BITalinoException(BITalinoErrorTypes.LOST_COMMUNICATION);
+    }
+  }
+
+}
---
+++ src/main/java/com/bitalino/BITalinoErrorTypes.java
@@ -0,0 +1,57 @@
+package com.bitalino;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TODO replace description strings with properties for internationalization.
+ */
+public enum BITalinoErrorTypes {
+  BT_DEVICE_NOT_CONNECTED(0, "Bluetooth Device not connected"),
+  PORT_COULD_NOT_BE_OPENED(
+      1,
+      "The communication port could not be initialized. The provided parameters could not be set."),
+  DEVICE_NOT_IDLE(2, "Device not in idle mode"),
+  DEVICE_NOT_IN_ACQUISITION_MODE(3, "Device not is acquisition mode"),
+  SAMPLING_RATE_NOT_DEFINED(4,
+      "The Sampling Rate chose cannot be set in BITalino. Choose 1000,100,10 or 1"),
+  LOST_COMMUNICATION(5, "The Computer lost communication"),
+  INVALID_PARAMETER(6, "INVALID_PARAMETER"),
+  THRESHOLD_NOT_VALID(7, "The threshold value must be between 0 and 63"),
+  ANALOG_CHANNELS_NOT_VALID(8,
+      "The number of analog channels available are between 0 and 5"),
+  INCORRECT_DECODE(9, "Incorrect data to be decoded"),
+  DIGITAL_CHANNELS_NOT_VALID(
+      10,
+      "To set the digital outputs, the input array must have 4 items, one for each channel."),
+  MACADDRESS_NOT_VALID(11, "MAC address not valid."),
+  UNDEFINED(12, "UNDEFINED ERROR");
+
+  private final int value;
+  private final String description;
+
+  private static Map<Integer, BITalinoErrorTypes> map = new HashMap<>();
+
+  static {
+    for (BITalinoErrorTypes error : BITalinoErrorTypes.values())
+      map.put(error.getValue(), error);
+  }
+
+  private BITalinoErrorTypes(final int value, final String description) {
+    this.value = value;
+    this.description = description;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public static BITalinoErrorTypes fromValue(final int value) {
+    return map.get(value);
+  }
+
+}
