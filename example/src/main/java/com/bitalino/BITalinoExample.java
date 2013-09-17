16a087246fc9338b3771ce3f36fb623591428582
Paulo Pires <pjpires@gmail.com>
2013-09-17 23:34:32 +0100

    Broke code-base into 'api' and 'example' modules. Tested with device in Linux an Mac OS. Refactored the API a little bit. Updated documentation.
    Signed-off-by: Paulo Pires <pjpires@gmail.com>

--- example/src/main/java/com/bitalino/BITalinoExample.java
+++ example/src/main/java/com/bitalino/BITalinoExample.java
@@ -1,3 +1,15 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.bitalino;
 
 import java.util.logging.Logger;
@@ -27,8 +39,9 @@ public class BITalinoExample {
     device.start();
 
     // read 300 samples
-    BITalinoFrame[] frames = device.read(2000);
-    logger.info("FRAMES: " + frames);
+    BITalinoFrame[] frames = device.read(10);
+    for (BITalinoFrame frame : frames)
+      logger.info("FRAME: " + frame.toString());
 
     // trigger digital outputs
     int[] digital = { 1, 1, 1, 1 };
